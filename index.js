f7b4f48effdf885376f80236a747163fb09dfe2a
Eugene Shcherbinock <eugene@Eugenes-MacBook-Pro.local>
2021-03-30 19:34:59 +0300

    test

--- index.js
+++ index.js
@@ -7,5 +7,3 @@ const app = express()
 
 const config = serverConfigs[process.env.NODE_ENV] || serverConfigs.development
 new DefaultServerSetupCommand(app, config).execute()
-
-console.log("test changes CI")
